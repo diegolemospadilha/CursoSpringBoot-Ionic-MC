6d0197e332c4d682658b01465e897681c29eb698
lemospadilha <dlemos.padilha@gmail.com>
2019-02-13 23:04:22 -0200

    Testando a primeira Classe de domínio.

---
+++ src/main/java/com/lemospadilha/curso/boot/resources/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.lemospadilha.curso.boot.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lemospadilha.curso.boot.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listaCategorias = new ArrayList<>();
+		listaCategorias.add(cat1);
+		listaCategorias.add(cat2);
+		
+		return listaCategorias;
+	}
+	
+}
