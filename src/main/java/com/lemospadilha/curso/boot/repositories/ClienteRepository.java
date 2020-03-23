fa80ca682ccc92967ee40e9dd941ecd9349d6f7a
lemospadilha <dlemos.padilha@gmail.com>
2020-03-23 15:00:51 -0300

    Validação customizada e-mail não repetido na inserção de cliente

--- src/main/java/com/lemospadilha/curso/boot/repositories/ClienteRepository.java
+++ src/main/java/com/lemospadilha/curso/boot/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.lemospadilha.curso.boot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lemospadilha.curso.boot.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
-
+	
+	@Transactional(readOnly = true)
+	public Cliente findByEmail(String email);
 }
