905d9853dd520711c90b5d96394b703089adfb0b
lemospadilha <dlemos.padilha@gmail.com>
2020-04-01 11:30:41 -0300

    feat: implementando autorização de token

--- src/main/java/com/lemospadilha/curso/boot/config/SecurityConfig.java
+++ src/main/java/com/lemospadilha/curso/boot/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.lemospadilha.curso.boot.security.JWTAuthenticationFilter;
+import com.lemospadilha.curso.boot.security.JWTAuthorizationFilter;
 import com.lemospadilha.curso.boot.security.JWTUtil;
 
 @Configuration
@@ -39,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET,PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
