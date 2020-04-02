33bd148aba01d421ddd101d7f58b3612dd52d0c4
lemospadilha <dlemos.padilha@gmail.com>
2020-04-02 12:12:24 -0300

    feat: add tratamento de exceções para envio de imagens

--- src/main/java/com/lemospadilha/curso/boot/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lemospadilha/curso/boot/resources/exceptions/ResourceExceptionHandler.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.lemospadilha.curso.boot.services.exceptions.AuthorizationException;
 import com.lemospadilha.curso.boot.services.exceptions.DataIntegrityException;
+import com.lemospadilha.curso.boot.services.exceptions.FileException;
 import com.lemospadilha.curso.boot.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -45,6 +49,30 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> authorizationIntegrity(AuthorizationException e, HttpServletRequest request){
 		StandardError error = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+	}
 	
+	@ExceptionHandler(FileException.class)
+	public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request){
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(AmazonServiceException.class)
+	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request){
+		HttpStatus code = HttpStatus.valueOf(e.getStatusCode());
+		StandardError error = new StandardError(code.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(code.value()).body(error);
+	}
+	
+	@ExceptionHandler(AmazonClientException.class)
+	public ResponseEntity<StandardError> amazonClient(AmazonClientException e, HttpServletRequest request){
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	@ExceptionHandler(AmazonS3Exception.class)
+	public ResponseEntity<StandardError> amazons3(AmazonS3Exception e, HttpServletRequest request){
+		StandardError error = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
--- src/main/java/com/lemospadilha/curso/boot/services/S3Service.java
+++ src/main/java/com/lemospadilha/curso/boot/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.lemospadilha.curso.boot.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -35,7 +36,7 @@ public class S3Service {
 			String contentType = multipartFile.getContentType();
 			return uploadFile(is, filename, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 
 	}
@@ -49,7 +50,7 @@ public class S3Service {
 			LOG.info("Enviando arquivo");
 			return s3client.getUrl(bucketName, filename).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao obter url");
+			throw new FileException("Erro ao obter url");
 		}
 	}
 
