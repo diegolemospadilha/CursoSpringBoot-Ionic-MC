1b923456830a074a5599a015eb16ee76b6c9c92a
lemospadilha <dlemos.padilha@gmail.com>
2020-04-02 11:58:11 -0300

    feat: add endpoint para adição de imagens de clientes ao seu perfil

--- src/main/java/com/lemospadilha/curso/boot/services/ClienteService.java
+++ src/main/java/com/lemospadilha/curso/boot/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.lemospadilha.curso.boot.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.lemospadilha.curso.boot.domain.Cidade;
 import com.lemospadilha.curso.boot.domain.Cliente;
@@ -39,6 +41,9 @@ public class ClienteService {
 	
 	@Autowired 
 	private EnderecoRepository enderecoRepo;
+	
+	@Autowired 
+	private S3Service s3Service;
 	
 	public Cliente findById(Integer id) {
 		
@@ -114,6 +119,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());	
+	}
+	
+	public URI uploadPicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 	
