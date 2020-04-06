3da95e1cc7be9b8837d187e6e7e0658a07aa7e31
lemospadilha <dlemos.padilha@gmail.com>
2020-04-06 12:21:51 -0300

    feat: config cors

--- src/main/java/com/lemospadilha/curso/boot/config/SecurityConfig.java
+++ src/main/java/com/lemospadilha/curso/boot/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.lemospadilha.curso.boot.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -62,7 +64,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 @Bean
 	  CorsConfigurationSource corsConfigurationSource() {
 	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+	    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+	    source.registerCorsConfiguration("/**", config);
 	    return source;
 	  }
 	 
